c8691f4147b1427f4b8ce359d3b0b9e53a1010d8
foril <1571825323@qq.com>
2023-06-20 15:16:57 +0800

    cors

--- src/application.ts
+++ src/application.ts
@@ -8,14 +8,28 @@ import aboutRouter from './routes/aboutRouter'
 const app = new Koa()
 const port = 8080
 
-// 装载所有路由
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*')
+  ctx.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
+  )
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200
+  } else {
+    await next()
+  }
+})
 
+// 装载所有路由
 const subRouter = new Router()
 subRouter.use(
   '/articleInfo',
   articleInfoRouter.routes(),
   articleInfoRouter.allowedMethods(),
 )
+
 subRouter.use(
   '/articleCate',
   articleCateRouter.routes(),
@@ -28,6 +42,7 @@ subRouter.use(
 )
 subRouter.use('/about', aboutRouter.routes(), aboutRouter.allowedMethods())
 
+// 挂载到 /api 路径下
 const router = new Router()
 router.use('/api', subRouter.routes(), subRouter.allowedMethods())
 
